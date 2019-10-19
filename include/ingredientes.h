612c9d9549aed017fe347633e7abf2fd38ebdb90
crisfj00 <crisfj00@correo.ugr.es>
2019-10-19 17:06:12 +0200

    actualizacion

---
+++ include/ingredientes.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ingredientes.h
+ * Author: cristian
+ *
+ * Created on 19 de octubre de 2019, 17:05
+ */
+
+#ifndef INGREDIENTES_H
+#define INGREDIENTES_H
+
+
+
+#endif /* INGREDIENTES_H */
+
