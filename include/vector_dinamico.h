1e1c3d4c950b44e3ca2bbda02981281bc1eed741
crisfj00 <crisfj00@correo.ugr.es>
2019-10-24 21:33:30 +0200

    muchos cambios

---
+++ include/vector_dinamico.h
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   vector_dinamico.h
+ * Author: cristian
+ *
+ * Created on 18 de octubre de 2019, 19:30
+ */
+
+#ifndef VECTOR_DINAMICO_H
+#define VECTOR_DINAMICO_H
+
+template <class T>
+class vector_dinamico {
+    private:
+        T *datos;
+        int reservados=0;
+        int util=0;
+        
+    public:
+        vector_dinamico(int n=100);
+        vector_dinamico(const vector_dinamico& original);
+        ~vector_dinamico();
+        int size() const;
+        int getUtil() const;
+        T get(int i) const;
+        void set(int i, T v);
+        void resize(int n);
+        vector_dinamico & operator=(const vector_dinamico& original);
+        void aniadir(T otro);
+        T& obtener(int i);
+        void insertar(int i, T otro);
+        void Liberar();
+};
+
+//#include "../src/vector_dinamico.cpp"
+#endif /* VECTOR_DINAMICO_H */
+
+
